e5a7b95469c4a6f40f2a9706d4eff10441caea0b
Khalil Elloumi <mohamedkhalil.elloumi@ensi-uma.tn>
2021-04-05 15:43:39 +0100

    change for heroku deployment

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/frontend"));
+app.use(express.static(__dirname + "/dist"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/src"));
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 console.log(app); // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4000);
