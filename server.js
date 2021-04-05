0c3a252a5c56399211a432f2c1f9a454f4b1a3bb
Khalil Elloumi <mohamedkhalil.elloumi@ensi-uma.tn>
2021-04-05 16:40:20 +0100

    deploy frontend will work

--- server.js
+++ server.js
@@ -11,9 +11,8 @@ const apiProxy = proxy("https://bot-mind.herokuapp.com/", {
 app.use("/api/*", apiProxy);
 app.use("/auth/*", apiProxy);
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/frontend"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
+  res.sendFile(path.join(__dirname + "/src"));
 });
-console.log(app); // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4000);
